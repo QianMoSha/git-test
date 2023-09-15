2ad3669a1c411b2010b65c4c39e4e36d8737b1a8
QianMoSha <lj18200217875@163.com>
2023-09-15 15:21:58 +0800

    fourth commit

--- src/main/java/com/qmstudyjava/git/GitTest.java
+++ src/main/java/com/qmstudyjava/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello git");
         System.out.println("Hello kitty");
         System.out.println("Hello java");
+        System.out.println("Hello java");
     }
 }
