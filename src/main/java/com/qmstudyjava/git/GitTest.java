cbc77e655ffa5cf3bac81795398331b84205a2fd
QianMoSha <lj18200217875@163.com>
2023-09-15 10:24:15 +0800

    first commit

---
+++ src/main/java/com/qmstudyjava/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.qmstudyjava.git;
+
+/**
+ * @author 浅墨洒
+ * @version 1.0
+ * @className GitTest
+ * @since 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
