272f67c9ce4879d0dcbef5a5036d0609f2a1ef37
QianMoSha <lj18200217875@163.com>
2023-09-15 10:27:26 +0800

    second commit

--- src/main/java/com/qmstudyjava/git/GitTest.java
+++ src/main/java/com/qmstudyjava/git/GitTest.java
@@ -9,5 +9,7 @@ package com.qmstudyjava.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello kitty");
     }
 }
