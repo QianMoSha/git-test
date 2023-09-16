bdb55b01238d3dd5c90311347a49f1daffaed2be
QianMoSha <lj18200217875@163.com>
2023-09-16 10:18:35 +0800

    push commit1

--- src/main/java/com/qmstudyjava/git/GitTest.java
+++ src/main/java/com/qmstudyjava/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello java");
         System.out.println("Hello hot-fix");
         System.out.println("version five");
+        System.out.println("push five");
     }
 }
